00036924d8c45ebe9e379676038c7b2532b511e5
Loura <loura@chalmers.se>
2024-11-28 15:40:12 +0100

    Push-AddBooks

---
+++ src/main/java/com/example/demo/model/Library.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+
+public class Library {
+    Book book;
+    ArrayList<Book> bookList= new ArrayList<>();
+    public void addBook(Book book) {
+        bookList.add(book);
+    }
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+}
+
