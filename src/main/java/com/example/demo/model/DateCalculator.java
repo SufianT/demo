5b1a0c709de93e9d48ab47f05768a0f6d2a4ca73
Sukaina <Sukaina_akar@hotmail.com>
2024-11-28 15:34:47 +0100

    Låneperiod/Bötessystem
    Låneperiod klass som anger låneperiod för en användare och en book + andra klasser som är nödvändiga för bötessystemet.

---
+++ src/main/java/com/example/demo/model/DateCalculator.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+public class DateCalculator { //Calculates due date and days between dates
+
+    public static LocalDate calculateDueDate(LocalDate startDate, int loanDurationDays){ 
+        return startDate.plusDays(loanDurationDays);
+    }
+
+    public static int calculateDaysBetween(LocalDate date1, LocalDate date2){
+        return (int) java.time.temporal.ChronoUnit.DAYS.between(date1, date2);
+    }
+    
+}
