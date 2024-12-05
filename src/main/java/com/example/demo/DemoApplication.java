091cd40813203881c558320f8ee417598b748c57
Sufian <sufian_tariq@outlook.com>
2024-12-05 14:56:46 +0100

    final initial

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.model.BookHandler;
-import com.example.demo.model.Library;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-
-			// Initialize the library
-		Library library = new Library();
-		BookHandler bookHandler = new BookHandler(library);
-
-		bookHandler.loadBooksFromDatabase("src/main/resources/JavaScript/Books.json");
-			// Print the books to verify
-			library.getBookList().forEach(book -> {
-				System.out.println("Title: " + book.getTitle() +
-						", ISBN: " + book.getISBN() +
-						", Authors: " + book.getAuthor() +
-						", Is Borrowed: " + book.getIsBarrowed());
-			});
-
+		SpringApplication.run(DemoApplication.class, args);
 	}
-
 
 }
