fd118fc5e35d84df5219204bfa230db66da79f59
King-of-Roses <86077882+King-of-Roses@users.noreply.github.com>
2024-12-09 16:35:42 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/model/SetOfBooksISBNInterface.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public interface SetOfBooksISBNInterface {
+    public Set<String> setOfBooksISBN(ArrayList<Book> books);
+}
