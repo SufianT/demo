5b1813032d520d15b1ec2947e5ba1a5af25bb185
King-of-Roses <86077882+King-of-Roses@users.noreply.github.com>
2024-12-09 16:52:09 +0100

    Update SearchEngine.java

--- src/main/java/com/example/demo/model/SearchEngine.java
+++ src/main/java/com/example/demo/model/SearchEngine.java
@@ -1,26 +1,41 @@
 package com.example.demo.model;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class SearchEngine implements SearchInterface {
-    ArrayList<Book> books = new ArrayList<>();
 
-    @Override
-    public ArrayList<Book> SearchByAuthor(String[] authors) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'SearchByAuthor'");
+    private StringToWantedWordsInterface wI;
+    private CalculateMostWantedBookInterface cI;
+    private SetOfBookstoStringInterface setTitleAuthor;
+    private SetOfBooksISBNInterface setISBN;
+
+    public SearchEngine(StringToWantedWordsInterface w, CalculateMostWantedBookInterface c)
+    {
+
+    }
+    public ArrayList<Book> SearchByAuthorAndTitle(String input, ArrayList<Book> books) {
+
+        return cI.sortHashMapByValue(cI.sortBooksFromSearch(wI.searchComplete(input,setTitleAuthor.setOfBookstoString(books),3),books));
+
     }
 
     @Override
-    public ArrayList<Book> SearchBytitle(String title) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'SearchBytitle'");
-    }
-
-    @Override
-    public ArrayList<Book> SearchByISBN(int ISBN) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'SearchByISBN'");
+    public ArrayList<Book> SearchByISBN(String input,ArrayList<Book> books) {
+        ArrayList<Book> bookFromISBNSearch = new ArrayList<>();
+        if (wI.searchComplete(input,setISBN.setOfBooksISBN(books),0).isEmpty()){
+            return bookFromISBNSearch;
+        }
+        for (String key : wI.searchComplete(input,setISBN.setOfBooksISBN(books),0).keySet()) {
+            String searchedISBN = key;
+            for (Book book : books){
+                if(book.getISBN()==searchedISBN) {
+                    bookFromISBNSearch.add(book);
+                    return bookFromISBNSearch;
+                }
+            }
+        }
+        return bookFromISBNSearch;
     }
 
 }
